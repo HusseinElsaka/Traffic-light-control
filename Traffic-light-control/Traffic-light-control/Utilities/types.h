44774747d98e0caa5b34cee560e7c29c3e0b5761
HusseinElsaka <ehussein366@gmail.com>
2022-08-12 22:08:58 +0200

    Utilities to help

---
+++ Traffic-light-control/Traffic-light-control/Utilities/types.h
@@ -0,0 +1,13 @@
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+
+typedef	unsigned char uint8_t;
+typedef	unsigned short uint16_t;
+typedef	unsigned long uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef enum Error {OK, ERROR}ERROR_H;
+
+#endif /* TYPES_H_ */
